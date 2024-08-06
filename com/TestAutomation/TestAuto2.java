372fc2981be395f6699d65983f80abb35b78ba48
Ramana Murthy <rama_70@yahoo.com>
2024-08-06 20:31:17 +0530

    save changes to  20224

--- com/TestAutomation/TestAuto2.java
+++ com/TestAutomation/TestAuto2.java
@@ -1,2 +1,8 @@
-package com.TestAutomation;public class TestAuto2 {
+package com.TestAutomation;
+
+public class TestAuto2 {
+
+    public static void main(String ar[]){
+        System.out.println("first git line update");
+    }
 }
